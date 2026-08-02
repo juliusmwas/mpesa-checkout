0de0a473a1273d0fdeb2739fec6bff9bd56c3a0f
juliusmwass <juliusmwass100@gmail.com>
2026-08-02 12:03:02 +0300

    updated the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the M-Pesa Backend API 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
